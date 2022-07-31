20202654505155f73eb9b239c87f3c5d879053ca
William Tam <wtam@redhat.com>
2022-07-31 00:47:24 -0400

    remove all build esbuild in node_pkgs

--- node_pkgs/argo-ui/v2/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './context/theme';
-export * from './keypress';
--- node_pkgs/timezones-list/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface TimeZone {
-    label: string;
-    tzCode: string;
-    name: string;
-    utc: string;
-}
-declare const _default: TimeZone[];
-export default _default;
